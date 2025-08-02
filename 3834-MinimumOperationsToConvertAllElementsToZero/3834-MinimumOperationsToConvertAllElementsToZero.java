2a0b13d99f742e84a548ddc854aff3049a9a1132
Teena Agarwal <teena53597@gmail.com>
2025-08-02 23:28:37 +0530

    Create 3834-MinimumOperationsToConvertAllElementsToZero

---
+++ 3834-MinimumOperationsToConvertAllElementsToZero/3834-MinimumOperationsToConvertAllElementsToZero.java
@@ -0,0 +1,23 @@
+// Last updated: 8/2/2025, 11:28:40 PM
+class Solution {
+    public int minOperations(int[] nums) {
+        // increasing stack
+        Stack<Integer> st = new Stack<>();
+        int ans=0;
+        for(int i=0 ; i<nums.length ; i++){
+            while(!st.isEmpty() && nums[st.peek()]>nums[i]){
+                st.pop();
+                ans++;
+            }
+            if(!st.isEmpty() && nums[st.peek()]==nums[i]){
+                continue;
+            }
+            st.push(i);
+        }
+        while(!st.isEmpty()){
+            if(nums[st.pop()]!=0)
+            ans++;
+        }
+        return ans;
+    }
+}
