1d077b5df8e2ae8dcc611409d01bc74a40abc991
Teena Agarwal <teena53597@gmail.com>
2025-08-02 23:32:19 +0530

    Create 1642-WaterBottles

---
+++ 1642-WaterBottles/1642-WaterBottles.cpp
@@ -0,0 +1,13 @@
+// Last updated: 8/2/2025, 11:32:22 PM
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int drink=numBottles;
+        int empty=numBottles;
+        while(empty>=numExchange){
+            drink = drink + (empty/numExchange);
+            empty = (empty/numExchange) + (empty%numExchange);
+        }
+        return drink;
+    }
+};
