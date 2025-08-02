5d5fa4ebf4dfc155a2fe09f098f71d77d0e70e26
Teena Agarwal <teena53597@gmail.com>
2025-08-02 23:34:59 +0530

    Create 507-PerfectNumber

---
+++ 507-PerfectNumber/507-PerfectNumber.java
@@ -0,0 +1,6 @@
+// Last updated: 8/2/2025, 11:35:02 PM
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        return (num==6 || num==28 || num==496 || num==8128 || num==33550336);
+    }
+}
