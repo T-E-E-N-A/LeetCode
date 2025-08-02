5e2f205c6920ddbb91f68a9fb9dd0784558ef531
Teena Agarwal <teena53597@gmail.com>
2025-08-02 23:57:08 +0530

    Create 31-NextPermutation

---
+++ 31-NextPermutation/31-NextPermutation.java
@@ -0,0 +1,43 @@
+// Last updated: 8/2/2025, 11:57:11 PM
+class Solution {
+    public void nextPermutation(int[] arr) {
+       int p = -1;
+		int n = arr.length;
+        // to find the next small element from last
+		for (int i = n - 2; i >= 0; i--) {
+			if (arr[i] < arr[i + 1]) {
+				p = i;
+				break;
+			}
+		}
+        // if next element small element not found then 321 is condition so reverse the array
+		if (p == -1) {
+			Reverse(arr, 0, n - 1);
+			return;
+		}
+		int q = -1;
+		for (int i = n - 1; i > p; i--) {
+			if (arr[i] > arr[p]) {
+				q = i;
+				break;
+			}
+		}
+		// swap p & q
+		int temp = arr[p];
+		arr[p] = arr[q];
+		arr[q] = temp;
+		Reverse(arr, p + 1, n - 1);
+
+	}
+
+	public static void Reverse(int[] arr, int i, int j) {
+
+		while (i < j) {// for(; i<j; i++,j--)
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+			i++;
+			j--;
+		} 
+    }
+}
