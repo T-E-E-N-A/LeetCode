4df099d638cf97db0512801731d1ef19879e860b
Teena Agarwal <teena53597@gmail.com>
2025-08-02 23:36:10 +0530

    Create 274-HIndex

---
+++ 274-HIndex/274-HIndex.java
@@ -0,0 +1,29 @@
+// Last updated: 8/2/2025, 11:36:13 PM
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int start=0,end=citations.length-1;
+        int ans=0,index=0;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(citations[mid]<=(citations.length-mid)){
+                if(ans<=citations[mid]){
+                    ans = citations[mid];
+                    index = mid;
+                }
+                start=mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+
+        if(citations[index]<(citations.length-index)){
+            if( (index+1)<citations.length && citations[index+1] >= (citations.length-index-1) )
+                ans = Math.max(ans,(citations.length-index-1));
+        }
+        if(citations[0]>=citations.length){
+            ans = citations.length;
+        }
+        return ans;
+    }
+}
