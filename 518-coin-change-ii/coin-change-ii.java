cdc7d6ec5dddf29fc7ac734fbf39ce9bdbb1c29d
Teena Agarwal <teena53597@gmail.com>
2025-09-05 17:59:53 +0530

    Time: 6 ms (74.54%) | Memory: 41.4 MB (82.27%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,36 +1,58 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int[][] dp = new int[amount+1][coins.length+1];
-        Arrays.fill(dp[0],1);
-        for(int i=1 ; i<dp.length ; i++){
-            for(int j=1 ; j<dp[0].length ; j++){
-                int a = 0;
-                if(i >= coins[j-1]){
-                    a = dp[i-coins[j-1]][j]; 
-                } 
-                dp[i][j] = a + dp[i][j-1];
+        int[] dp = new int[amount+1];
+        dp[0]=1;
+
+        // here i is coins index
+        for(int i=0 ; i<coins.length ; i++){
+            // int a=dp[0];
+            for(int am=1 ; am<dp.length ; am++){
+                int inc=0,exc=0;
+                if(am>=coins[i]){
+                    // removed last coin for that dp
+                    inc = dp[am-coins[i]];
+                }
+                dp[am] += inc;
             }
         }
-        return dp[dp.length-1][dp[0].length-1];
+        return dp[dp.length-1];
     }
-    /*
-    public int change(int amount, int[] coins) {
-        int[][] dp = new int[amount][coins.length];
-        for(int[] dpi : dp){
-            Arrays.fill(dpi,-1);
-        }
-        return fun(amount,coins,0,0,dp);
-    }
-    public int fun(int amount,int[] coins,int i,int j, int[][] dp){
-        if(i==amount) return 1;
-        if(j==coins.length) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int ans=0;
-        if(i+coins[j]<=amount){
-            ans =  fun(amount,coins,i+coins[j],j,dp);
-        }
-        ans += fun(amount,coins,i,j+1,dp);
-        dp[i][j] = ans;
-        return ans;
-    }*/
 }
+
+
+// class Solution {
+//     public int change(int amount, int[] coins) {
+//         int[][] dp = new int[amount+1][coins.length+1];
+//         Arrays.fill(dp[0],1);
+//         for(int i=1 ; i<dp.length ; i++){
+//             for(int j=1 ; j<dp[0].length ; j++){
+//                 int a = 0;
+//                 if(i >= coins[j-1]){
+//                     a = dp[i-coins[j-1]][j]; 
+//                 } 
+//                 dp[i][j] = a + dp[i][j-1];
+//             }
+//         }
+//         return dp[dp.length-1][dp[0].length-1];
+//     }
+//     /*
+//     public int change(int amount, int[] coins) {
+//         int[][] dp = new int[amount][coins.length];
+//         for(int[] dpi : dp){
+//             Arrays.fill(dpi,-1);
+//         }
+//         return fun(amount,coins,0,0,dp);
+//     }
+//     public int fun(int amount,int[] coins,int i,int j, int[][] dp){
+//         if(i==amount) return 1;
+//         if(j==coins.length) return 0;
+//         if(dp[i][j]!=-1) return dp[i][j];
+//         int ans=0;
+//         if(i+coins[j]<=amount){
+//             ans =  fun(amount,coins,i+coins[j],j,dp);
+//         }
+//         ans += fun(amount,coins,i,j+1,dp);
+//         dp[i][j] = ans;
+//         return ans;
+//     }*/
+// }
