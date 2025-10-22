ab202691835e1d597a826842d0a72bb8c70ba9cd
Teena Agarwal <teena53597@gmail.com>
2025-10-22 23:37:31 +0530

    Time: 7 ms (98.08%) | Memory: 45.2 MB (75.26%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.java
+++ 227-basic-calculator-ii/basic-calculator-ii.java
@@ -1,31 +1,70 @@
+// CAN I DO IT WITHOUT STACK = YES!!!!
+// we r using stack just for addition and retreiving last element only so i used a variable for that
 class Solution {
     public int calculate(String s) {
-        Stack<Integer> st = new Stack<>();
+        int ans=0;
+        int last=0;
         int num=0;
         char prev='+';
         for(char ch : s.toCharArray()){
             if(Character.isDigit(ch)) num = num*10 + ch-'0';
             else if(ch!=' '){
-                if(prev=='+') st.push(num);
-                else if(prev=='-') st.push(-num);
-                else if(prev=='*' && !st.isEmpty()){
-                    st.push(st.pop()*num);
-                }else if(prev=='/' && !st.isEmpty()){
-                    st.push(st.pop()/num);
+                if(prev=='+'){
+                    ans += last;
+                    last = num;
                 }
+                else if(prev=='-'){
+                    ans += last;
+                    last = -num;
+                }
+                else if(prev=='*') last = last*num;
+                else if(prev=='/') last = last/num;
+
                 prev = ch;
                 num=0;
             }
         }
-        if(prev=='+') st.push(num);
-        else if(prev=='-') st.push(-num);
-        else if(prev=='*' && !st.isEmpty()){
-            st.push(st.pop()*num);
-        }else if(prev=='/' && !st.isEmpty()){
-            st.push(st.pop()/num);
-        }
-        int ans=0;
-        while(!st.isEmpty()) ans+= st.pop();
-        return ans;
+        if(prev=='+'){
+            ans += last;
+            last = num;
+        }else if(prev=='-'){
+            ans += last;
+            last = -num;
+        }else if(prev=='*') last = last*num;
+        else if(prev=='/') last = last/num;
+
+        return ans+last;
     }
 }
+
+// class Solution {
+//     public int calculate(String s) {
+//         Stack<Integer> st = new Stack<>();
+//         int num=0;
+//         char prev='+';
+//         for(char ch : s.toCharArray()){
+//             if(Character.isDigit(ch)) num = num*10 + ch-'0';
+//             else if(ch!=' '){
+//                 if(prev=='+') st.push(num);
+//                 else if(prev=='-') st.push(-num);
+//                 else if(prev=='*' && !st.isEmpty()){
+//                     st.push(st.pop()*num);
+//                 }else if(prev=='/' && !st.isEmpty()){
+//                     st.push(st.pop()/num);
+//                 }
+//                 prev = ch;
+//                 num=0;
+//             }
+//         }
+//         if(prev=='+') st.push(num);
+//         else if(prev=='-') st.push(-num);
+//         else if(prev=='*' && !st.isEmpty()){
+//             st.push(st.pop()*num);
+//         }else if(prev=='/' && !st.isEmpty()){
+//             st.push(st.pop()/num);
+//         }
+//         int ans=0;
+//         while(!st.isEmpty()) ans+= st.pop();
+//         return ans;
+//     }
+// }
