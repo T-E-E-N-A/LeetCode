077d2641942305644e2873951add81664902ce94
Teena Agarwal <teena53597@gmail.com>
2025-11-15 00:04:10 +0530

    Time: 29 ms (7.33%) | Memory: 47.5 MB (5.48%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,37 +1,42 @@
 class Solution {
     // A graph is bipartite if the nodes can be partitioned into two independent sets A and B such that every edge in the graph connects a node in set A and a node in set B.
     public boolean isBipartite(int[][] graph) {
-        int[] vis = new int[graph.length];
-        for(int i=0 ; i<vis.length ; i++){
-            if(vis[i]==0){
-                if(!bfs(graph,vis,i)){
-                    return false;
+        // solve it by detecting cycle length is odd or not
+        // as bipartite graph cannot have odd length cycle
+        HashMap<Integer,Integer> vis = new HashMap<>();
+        for(int i=0 ; i<graph.length ; i++){
+            if(!vis.containsKey(i)){
+                if(!fun(graph,vis,i)) return false;
+            }
+        }
+        return true;
+    }
+    public boolean fun(int[][] graph, HashMap<Integer,Integer> vis, int i){
+        Queue<pair> qu = new LinkedList<>();
+        qu.add(new pair(i,0));
+        while(!qu.isEmpty()){
+            // remove
+            pair x = qu.poll();
+
+            if(vis.containsKey(x.vtx)){
+                if(vis.get(x.vtx)!=x.dist) return false;
+            }
+
+            vis.put(x.vtx,x.dist);
+            for(int nbrs : graph[x.vtx]){
+                if(!vis.containsKey(nbrs)){
+                    qu.add(new pair(nbrs,x.dist+1));
                 }
             }
         }
         return true;
     }
-    public boolean bfs(int[][] graph, int[] vis,int i){
-        Stack<pair> qu = new Stack<>();
-        qu.push(new pair(-1,i));
-        while(!qu.isEmpty()){
-            pair x = qu.pop();
-            int color = 0-x.col;
-            vis[x.i] = x.col;
-            for(int neigh : graph[x.i]){
-                if(vis[neigh]==0){
-                    qu.push(new pair(color,neigh));
-                }else if(vis[neigh]!=color) return false;
-            }
-        }
-        return true;
-    }
     class pair{
-        int col;
-        int i;
-        public pair(int color,int val){
-            col=color;
-            i = val;
+        int vtx;
+        int dist;
+        public pair(int vtx,int dist){
+            this.vtx = vtx;
+            this.dist = dist;
         }
     }
 }
