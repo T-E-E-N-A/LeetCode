2898b5145ca6d0019be7eb344fa9dd7e5717d672
Teena Agarwal <teena53597@gmail.com>
2025-12-06 17:06:01 +0530

    Time: 106 ms (5.48%) | Memory: 47 MB (44.52%) - LeetSync

--- 1613-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.java
+++ 1613-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.java
@@ -1,9 +1,6 @@
-// TIME COMPLEXITY = E + Elog(E) + 2*(E+N) + E*4(E+N)
-// overall TC : O(ElogE + E*E + E*N)
 class Solution {
-    // graph-LL
-    // 0idx-e1(from) 1idx-e2(to) 2 idx-cost 3idx-i(real idx of edges array)
     public List<List<Integer>> findCriticalAndPseudoCriticalEdges(int n, int[][] edges) {
+        // step 1 : create Graph
         int[][] ll = new int[edges.length][4];
         for(int i=0 ; i<edges.length ; i++){
             ll[i][0]=edges[i][0];
@@ -11,9 +8,7 @@ class Solution {
             ll[i][2]=edges[i][2];
             ll[i][3]=i;
         }
-
         // step 2 : find minCost
-        // sort on base of cost
         Arrays.sort(ll,(a,b)->a[2]-b[2]);
         int minimumCost = kruskal(ll,n,-1,true);
 
@@ -37,17 +32,14 @@ class Solution {
         return ans;
     }
 
-    // TC-2*(E+N)
     public int kruskal(int[][] ll,int n,int check,boolean flag){
-        // used dsu class
         DisJoinSet dsu = new DisJoinSet();
 		for (int v=0 ; v<n ; v++) dsu.create(v);
 		int ans = 0;
         if(!flag){
-            // find entry with idx check
+            // find entry with idx check & add that edge
             for(int[] e : ll){
                 if(e[3]==check){
-                    // add that to dsu
                     int re1 = dsu.find(e[0]);
                     int re2 = dsu.find(e[1]);
                     if (re1 != re2) {
@@ -71,8 +63,6 @@ class Solution {
         return ans;
     }
 
-    // O(1)- find create union
-    // O(N)- noMST
     class DisJoinSet{
         class Node{
             int val;
@@ -111,8 +101,6 @@ class Solution {
                 re2.parent = re1;
             }
         }
-
-        // tc : n
         public boolean noMST(){
             Node par = find(map.get(0));
             for(int k : map.keySet()){
@@ -122,72 +110,3 @@ class Solution {
         }
     }
 }
-
-/*
-
-    TRIED BY PRIMS BUT NOT POSSIBLE😭😭
-    KRUSKALS WORKS WITH VERTEX, DSU works for edges
-
-class Solution {
-    public List<List<Integer>> findCriticalAndPseudoCriticalEdges(int n, int[][] edges) {
-        // lets use brute force 
-        // let's delete every edge to check if it's critical or not
-        // i am going to do it firstly by prim's(easy than kruskal's)
-        // step 1 : create graph 
-        List<HashMap<Integer,Integer>> graph = new ArrayList<>();
-        graphAdded(graph,edges,n);
-
-        // step 2 : find minCost
-        int minimumCost = minCost(graph,n);
-
-        List<List<Integer>> ans = new ArrayList<>();
-        ans.add(new ArrayList<>());     //critical
-        ans.add(new ArrayList<>());     //pseudo-critical
-
-        // step 3 : start deleting edge if minimumCost changes #critical else #non-critical
-        for(int i=0 ; i<edges.length ; i++){
-            // delete Ith index edge then check mst cost
-            deleteEdge(graph,edges[i]);
-            int min = minCost(graph,n);
-            if(min==minimumCost) ans.get(1).add(i);
-            else ans.get(0).add(i);
-            addEdge(graph,edges[i]);
-        }
-        return ans;
-    }
-    public void deleteEdge(List<HashMap<Integer,Integer>> graph, int[] edge){
-        graph.get(edge[0]).remove(edge[1]);
-        graph.get(edge[1]).remove(edge[0]);
-    }
-    public void addEdge(List<HashMap<Integer,Integer>> graph, int[] edge){
-        graph.get(edge[0]).put(edge[1],edge[2]);
-        graph.get(edge[1]).put(edge[0],edge[2]);
-    }
-
-    public int minCost(List<HashMap<Integer,Integer>> graph , int n){
-        boolean[] vis = new boolean[n];
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[1]-b[1]);
-        pq.add(new int[]{0,0});
-        int cost=0;
-        while(!pq.isEmpty()){
-            int[] x = pq.poll();
-            if(vis[x[0]]) continue;
-            vis[x[0]] = true;
-            cost += x[1];
-            for(int nbr : graph.get(x[0]).keySet()){
-                if(vis[nbr]) continue;
-                pq.add(new int[]{nbr,graph.get(x[0]).get(nbr)});
-            }
-        }
-        return cost;
-    }
-    public void graphAdded(List<HashMap<Integer,Integer>> graph, int[][] edges,int n){
-        for(int i=0 ; i<n ; i++) graph.add(new HashMap<>());
-        // bidirectional graph as MST
-        for(int[] edge : edges){
-            graph.get(edge[0]).put(edge[1],edge[2]);
-            graph.get(edge[1]).put(edge[0],edge[2]);
-        }
-    }
-}
-*/
