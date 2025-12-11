a1a07fcb6fa98de9d1d57ba716d2ae1fde263a82
Teena Agarwal <teena53597@gmail.com>
2025-12-11 14:33:40 +0530

    Time: 28 ms (72.63%) | Memory: 230.8 MB (53.69%) - LeetSync

--- 3819-count-covered-buildings/count-covered-buildings.java
+++ 3819-count-covered-buildings/count-covered-buildings.java
@@ -1,27 +1,21 @@
 class Solution {
+    static int[][] store;
     public int countCoveredBuildings(int n, int[][] buildings) {
-        int[][] store = new int[n+1][2];
+        store = new int[n+1][4];
         for(int[] i : store){
             i[0]  = Integer.MAX_VALUE;
+            i[2]  = Integer.MAX_VALUE;
         }
-        int[][] storey = new int[n+1][2];
-        for(int[] i : storey){
-            i[0]  = Integer.MAX_VALUE;
-        }
-        // 0-index min 1-index max;
+        // 0,2-index min 1,3-index max;
         for(int[] ar : buildings){
             store[ar[0]][0] = Math.min(store[ar[0]][0],ar[1]);
             store[ar[0]][1] = Math.max(store[ar[0]][1],ar[1]);
-        }
-        // 0-index min 1-index max;
-        for(int[] ar : buildings){
-            storey[ar[1]][0] = Math.min(storey[ar[1]][0],ar[0]);
-            storey[ar[1]][1] = Math.max(storey[ar[1]][1],ar[0]);
+            store[ar[1]][2] = Math.min(store[ar[1]][2],ar[0]);
+            store[ar[1]][3] = Math.max(store[ar[1]][3],ar[0]);
         }
         int ans=0;
         for(int[] a : buildings){
-            // upper neeche check but how left right??
-            if(store[a[0]][0]<a[1] && store[a[0]][1]>a[1] && storey[a[1]][0]<a[0] && storey[a[1]][1]>a[0]) ans++;
+            if(store[a[0]][0]<a[1] && store[a[0]][1]>a[1] && store[a[1]][2]<a[0] && store[a[1]][3]>a[0]) ans++;
             
         }
         return ans;
