8c0f1f83fc2af7874d0e4cdc48906252fe593e72
Teena Agarwal <teena53597@gmail.com>
2025-12-18 22:36:40 +0530

    Time: 4 ms (99.34%) | Memory: 107 MB (16.78%) - LeetSync

--- 3980-best-time-to-buy-and-sell-stock-using-strategy/best-time-to-buy-and-sell-stock-using-strategy.java
+++ 3980-best-time-to-buy-and-sell-stock-using-strategy/best-time-to-buy-and-sell-stock-using-strategy.java
@@ -1,26 +1,23 @@
 class Solution {
     public long maxProfit(int[] prices, int[] strategy, int k) {
-        int n=prices.length;
-        long[] prefix = new long[n+1];
-        prefix[1] = prices[0]*strategy[0];
-        for(int i=1 ; i<n ; i++){
-            prefix[i+1] = prefix[i] + (prices[i]*strategy[i]);
-        }
-        long ans=prefix[n];
-        if(k>n) return ans;
-        int st=0,j=k/2;
-        long sum=0;
-        for(int i=k/2 ; i<k && i<prices.length ; i++){
-            sum += prices[i];
-        }
-        while(j+(k/2)<=n){
-            long suffix = prefix[st]-prefix[0];
-            long pre = prefix[n]-prefix[k+st];
-            ans = Math.max(ans,sum+suffix+pre);
-            sum -= prices[j];
-            if(j+(k/2)<n) sum += prices[j+(k/2)];
-            j++;
-            st++;
+        long sum=0,ans = 0;
+        for(int i=0;i<prices.length ; i++) ans += prices[i]*strategy[i];
+        for(int i=k/2 ;i<prices.length && i<k ; i++) sum += prices[i];
+        for(int i=k ; i<prices.length ; i++) sum += (prices[i]*strategy[i]);
+        
+
+        int start=0,mid=k/2,end=k;
+        ans = Math.max(ans,sum);
+        while(end<strategy.length){
+            sum -= prices[mid];
+            sum -= (prices[end]*strategy[end]);
+            sum += (prices[start]*strategy[start]);
+            sum += prices[end];
+
+            start++;
+            mid++;
+            end++;
+            ans = Math.max(sum,ans);
         }
         return ans;
     }
