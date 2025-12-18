ef6910d21cca5fd00be72c40495da3192ab30f58
Teena Agarwal <teena53597@gmail.com>
2025-12-18 23:00:25 +0530

    Time: 5 ms (77.38%) | Memory: 77.6 MB (15.26%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -13,30 +13,3 @@ class Solution {
         return ans;
     }
 }
-
-
-/*
-class Solution {
-    public int maxArea(int[] height) {
-        int n = height.length;
-        Stack<Integer> st = new Stack<>();
-        int ans=0;
-        for(int i=0 ; i<n ; i++){
-            while(!st.isEmpty() && height[st.peek()]<height[i]){
-                ans = Math.max(ans,(i-st.peek())*Math.min(height[i],height[st.peek()]));
-                st.pop();
-            }
-            st.push(i);
-        }
-        
-        while(!st.isEmpty()){
-            int i = st.pop();
-            if(!st.isEmpty())
-                ans = Math.max(ans,(i-st.peek())*height[i]);
-            else
-                ans = Math.max(ans,height[i]*(i));
-        }
-        return ans;
-    }
-}
-*/
