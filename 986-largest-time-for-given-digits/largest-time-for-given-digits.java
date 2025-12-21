2e442bc3fda4c2f7d22d5592bef2c0ea0273fe8a
Teena Agarwal <teena53597@gmail.com>
2025-12-21 19:38:33 +0530

    Time: 4 ms (90.31%) | Memory: 43.1 MB (64.32%) - LeetSync

--- 986-largest-time-for-given-digits/largest-time-for-given-digits.java
+++ 986-largest-time-for-given-digits/largest-time-for-given-digits.java
@@ -4,21 +4,18 @@ class Solution {
         for(int i=0 ; i<4 ; i++){
             for(int j=0 ; j<4 ; j++){
                 if(i==j) continue;
-                int x = arr[i]*10+arr[j];
-                if(x>23) continue;
-
+                int hh = arr[i]*10+arr[j];
+                if(hh>23) continue;
                 for(int k=0 ; k<4 ; k++){
                     if(k==i || k==j) continue;
-
-                    int l = 6-i-j-k;
+                    int l = (3+2+1) - i - j - k;
                     int mm = arr[k]*10+arr[l];
                     if(mm>59) continue;
-
-                    max = Math.max(max,x*60+mm);
+                    max = Math.max(max,hh*60 + mm);
                 }
             }
         }
-        if (max == -1) return "";
-        return String.format("%02d:%02d", max / 60, max % 60);
+        if(max==-1) return "";
+        return String.format("%02d:%02d",max/60,max%60);
     }
 }
